ae3449c18e2c89d69232feaccd40882d47ed25e2
MATSUMOTO-Takashi <t.matsumoto0308@gmail.com>
2014-06-26 20:16:14 +0900

    コントローラのテストの実施。 モックService層での単体テストと、Service層完成後の結合テストを実施。 結合テスト時にはservlet-context.xmlをテスト用に作っておいた方が便利。

---
+++ spring_mvc_unittest/src/main/java/local/tmatsumoto/app/web/form/SearchForm.java
@@ -0,0 +1,15 @@
+package local.tmatsumoto.app.web.form;
+
+public class SearchForm {
+
+	private String id;
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+	
+}
