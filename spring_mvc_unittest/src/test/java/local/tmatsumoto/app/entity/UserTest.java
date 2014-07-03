115b1cc4797e52f658affc2276f243eadb997237
MATSUMOTO-Takashi <t.matsumoto0308@gmail.com>
2014-07-03 20:16:41 +0900

    UserTestのリファクタリング。 共通処理を@Beforeに括りだした。

--- spring_mvc_unittest/src/test/java/local/tmatsumoto/app/entity/UserTest.java
+++ spring_mvc_unittest/src/test/java/local/tmatsumoto/app/entity/UserTest.java
@@ -5,18 +5,23 @@ import static org.junit.Assert.assertThat;
 import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.spy;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class UserTest {
 	
-	User user = new User();
+	User user;
+	Book book;
+	
+	@Before
+	public void setUp() {
+		user = spy(new User());
+		book = spy(new Book());
+	}
 
 	@Test
 	public void userの年齢が18歳以上ならR18も読める() {
-		User user = spy(this.user);
 		doReturn(18).when(user).getAge();
-		
-		Book book = spy(new Book());
 		doReturn(true).when(book).isR18();
 		
 		assertThat(user.canRead(book), is(true));
@@ -24,10 +29,7 @@ public class UserTest {
 	
 	@Test
 	public void userの年齢が18歳未満だとR18は読めない() {
-		User user = spy(this.user);
 		doReturn(17).when(user).getAge();
-		
-		Book book = spy(new Book());
 		doReturn(true).when(book).isR18();
 
 		assertThat(user.canRead(book), is(false));
@@ -35,10 +37,7 @@ public class UserTest {
 	
 	@Test
 	public void userの年齢に関係なくR18でなければ読める() {
-		User user = spy(this.user);
 		doReturn(17).when(user).getAge();
-		
-		Book book = spy(new Book());
 		doReturn(false).when(book).isR18();
 
 		assertThat(user.canRead(book), is(true));
